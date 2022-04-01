245e57323de3c1eb1cef25849e5c1836c0a699e8
Mr-Robot-83 <scott.bonanno@gmail.com>
2022-04-01 17:14:30 +1100

    some new UI and extra insult logic

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # rock-paper-scissors
-Rock paper scissors game as part of the Odin Project.
-Added a bunch of swears.
+Rock paper scissors game as part of the Odin Project with a bunch of swears.
--- scripts.js
+++ scripts.js
@@ -2,34 +2,64 @@
 const messageBox = document.getElementById("comp-message");
 
 const computerOptions = ["rock", "paper", "scissors"];
+
+const preRoundInsults = [
+  "dick teeth.",
+  "piss mouth.",
+  "fart balls.",
+  "fart face."
+];
+
+const computerAlmostWinning = [
+  "hahahahaha. What the actual fuck. You are trash. 2 v 0 lol. Fuck you.",
+  "I just need one more bitch. You are fucking trash lol."
+];
+
+const drawMessages = [
+  "you stupid fuck. You want another go?.. try me dick head.", 
+  "you dumb fuck. Try again.", 
+  "fuck head. What are you doing lol!",
+  "shit for brains. Go again.",
+  "whoopdifuckingdoodle. Try again."
+];
+
+
+const playerAlmostWinning = [
+  "... ok so you're pretty close. How about you chill and we be friends and forget this whole thing?",
+  "... oh shit big fucking guy over here. Whatever get fucked."
+
+];
+
+const finalRound = [
+  "so now we're even and it's the last round. I'm probably going to cheat so you should maybe just fuck off now?",
+  "and this is the last round. Look we both did pretty well but I'm a computer so if you win I'm probably going to hack your credit cards and purchase a shit tonne of porn and get it sent to your work or some shit so maybe you should drop this and go play Wordle or croquet or whatever the fuck humans do before shit gets serious."
+];
+
 const computerWinMessages = [ 
   "I win fuck face. Why don't you go try to eat a pile of steaming horse shit. 💩 💩 💩 💩 💩", 
   "💩 💩 Game over, I win dick head! 💩 💩",
   "I win you stupid fuck 💩",
   "🖕 I win. Eat shit. 💩",
   "You loose cock head 8===D~~~",
-  "🍆 🍆  You lose dick hole. Now for fuck's sake please go fuck yourself ok? 🍆 🍆",
+  "You lose dick hole!!!",
   "You lose shit for brains 💩 🧠 💩 🧠 💩",
   "🖕🖕🖕 I win, get fucked.🖕🖕🖕",
+  "Fuck you I win, get fucked. GG EZ EZ EZ EZ 🖕🖕🖕",
   "I win, fuck off.🖕 💩 🖕 💩 "
 ];
 
 const computerInsults = [ 
   "... fuckadoodle doo.", 
-  "you ugly fucking pile of mouth breathing pig piss.", 
-  "you barely concious cunt faced turd fuck.", 
+  "you stupid pile of mouth breathing pig piss.", 
+  "you barely concious cunt faced fuck turd.", 
   "so how about you go get fucked you fucking cock faced, fuck nut, prick fuck.",
-  "you stupid fucking dumb fuck.", 
-  "you dumb fuck.", 
-  "fuck head.",
-  "shit for brains.",
   "you mother fucking cock cunt.",
   "fuck nuts.",
   "cock head.",
+  "cock face.",
   "you dumb fucking dog ass cunt.",
   "you fucking shit cunt.",
   "dick hole.",
-  "piss mouth.",
   "you steaming bag of shit covered ball sacks.",
   "you stupid fucking cunting prick.",
   "you fucking brainless cunt."
@@ -55,9 +85,9 @@ function playRound(playerSelection, computerSelection) {
     roundNumber++;
     messageBox.innerHTML = "<br>" + messageBox.innerHTML;
     displayText(`Your score is ${playerScore}. My score is ${computerScore}.`);
-    displayText("💩 It's a draw "+computerRandomChoice(computerInsults));
+    displayText("💩 It's a draw "+computerRandomChoice(drawMessages));
     displayText(playedMessage);
-    displayRound(roundNumber);
+    displayRound(roundNumber, playerSelection, computerSelection);
   } else if (
       (play === "rock") && (computerSelection === "scissors") ||
       (play === "paper") && (computerSelection === "rock") ||
@@ -67,17 +97,17 @@ function playRound(playerSelection, computerSelection) {
     roundNumber++;
     messageBox.innerHTML = "<br>" + messageBox.innerHTML;
     displayText(`Your score is ${playerScore}. My score is ${computerScore}.`)  ;
-    displayText("🤬 You win round "+roundNumber+" "+computerRandomChoice(computerInsults));
+    displayText("🤬 You win round "+roundNumber+" "+computerRandomChoice(chooseMessageArray()));
     displayText(playedMessage);
-    displayRound(roundNumber);
+    displayRound(roundNumber, playerSelection, computerSelection);
   } else {
     ++computerScore;
     roundNumber++;
     messageBox.innerHTML = "<br>" + messageBox.innerHTML;
     displayText(`Your score is ${playerScore}. My score is ${computerScore}.`);
-    displayText("🤣 You lose round "+roundNumber+" "+computerRandomChoice(computerInsults));
+    displayText("🤣 You lose round "+roundNumber+" "+computerRandomChoice(chooseMessageArray()));
     displayText(playedMessage);
-    displayRound(roundNumber);
+    displayRound(roundNumber, playerSelection, computerSelection);
   }
 }
 
@@ -91,7 +121,7 @@ playScissors.addEventListener('click', () => playerSelects("scissors"));
 
 function playerSelects (choice) {
   if (computerScore === 0 && playerScore === 0 && roundNumber === -1) {
-    messageBox.innerHTML = "<span style='color:#50fa7b;'>Ok "+computerRandomChoice(computerInsults)+" First to 3, let's go.</span>";
+    messageBox.innerHTML = "<span style='color:#50fa7b;'>Ok "+computerRandomChoice(chooseMessageArray())+" First to 3, let's go.</span>";
     playRock.innerHTML = "👊";
     playPaper.innerHTML = "✋";
     playScissors.innerHTML = "✌️";
@@ -113,7 +143,7 @@ function gameOver () {
     result = computerRandomChoice(computerWinMessages);
   }
   messageBox.innerHTML = '<span style="color:#50fa7b;">' + result + '</span>' + "<br>" +
-                '<span style="color:#ff79c6;">Click 🖕 to start again...</span>' + "<br><br>" + messageBox.innerHTML;
+    '<span style="color:#ff79c6;">Click 🖕 to start again...</span>' + "<br><br>" + messageBox.innerHTML;
   computerScore = 0;
   playerScore = 0;
   roundNumber = -1;
@@ -122,10 +152,59 @@ function gameOver () {
   playScissors.innerHTML = "🖕";
 };
 
+//Choose which array to pick a message from based on the current score.
+//Note that draw rounds don't use this logic.
+function chooseMessageArray() {
+  if (computerScore === 0 && playerScore === 0 && roundNumber === -1) {
+    return preRoundInsults
+  } else if (playerScore === 2 && computerScore < 2) { 
+    return playerAlmostWinning
+  } else if (playerScore === 0 && computerScore === 2) { 
+    return computerAlmostWinning
+  } else if (playerScore === 2 && computerScore === 2) { 
+    return finalRound
+  } else {
+    return computerInsults
+  };
+};
+
 function displayText (message) {
   messageBox.innerHTML = message  + "<br>" +  messageBox.innerHTML;
 };
 
-function displayRound (roundNumber) {
-  messageBox.innerHTML = "<span style='color:#8be9fd;'>Round " + roundNumber + "</span>" + "<br>" + messageBox.innerHTML;
+function displayRound (roundNumber, playerSelection, computerSelection) {
+  messageBox.innerHTML = "<span style='color:#8be9fd;'>Round " + roundNumber +
+   displaySymbolsPlayed (playerSelection,computerSelection) +
+   "</span>" + "<br>" + messageBox.innerHTML;
 }
+
+//Convert the choice to an emoji and output a string to display next to each round number
+function displaySymbolsPlayed (player, computer) {
+  let playerSymbol;
+  let computerSymbol;
+  switch (player) {
+    case "rock":
+      playerSymbol = "👊"
+      break;
+    case "paper":
+      playerSymbol = "✋"
+      break;
+    case "scissors":
+      playerSymbol = "✌️"
+      break;
+  }
+  switch (computer) {
+    case "rock":
+      computerSymbol = "👊"
+      break;
+    case "paper":
+      computerSymbol = "✋"
+      break;
+    case "scissors":
+      computerSymbol = "✌️"
+      break;
+  }
+  return ": " + playerSymbol +" vs "+ computerSymbol;
+}
+
+
